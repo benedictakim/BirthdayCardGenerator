00abc05fc6d94ac86783f084e0d420266b0989a7
Kim Credit <kim@kimcredit.com>
2020-07-08 18:50:45 -0400

    for apis resulting in images, used results to update card results

--- nyt.js
+++ nyt.js
@@ -4,26 +4,31 @@ var inputMonth = $("#input-month").val();
             //user input year (typed)
             var inputYear = $("#input-year").val();
 
-// var pubdate = 19991231 YYYYMMDD
-var headlinequeryURL = "https://api.nytimes.com/svc/search/v2/articlesearch.json?begin_date="
-+ inputYear + inputMonth + inputDay + "&end_date="
-+ inputYear + inputMonth + inputDay + "&fq=print_page:1&print_section:A&api-key=l150AJIrBZQqHoU50PKwaI3cMa29A9da";
+function getHeadlines() {
+            var headlineArea = $("<div>");
+            var headlines = $("<h5>");
+            var links = $("<a>");
 
-$(document).ready(function(){
-    function headlines() {
-        $.ajax({
-            method:"GET",  
-            url: headlinequeryURL,
-        }).then(function(response){
-            console.log(response)
-            for (var i=0; i<10; i++) {
-                var asection = response.response.docs[i].print_section
-                //10 headlines on page 1 of sections A thru E etc
-                var printheadline = response.response.docs[i].headline.print_headline
-                console.log(printheadline)  
-            }
-        });
-    }
+            var headlinequeryURL = "https://api.nytimes.com/svc/search/v2/articlesearch.json?begin_date="
+                + inputYear + inputMonth + inputDay + "&end_date="
+                + inputYear + inputMonth + inputDay + "&fq=print_page:1&print_section:A&api-key=l150AJIrBZQqHoU50PKwaI3cMa29A9da";
+    
+            $.ajax({
+                method:"GET",  
+                url: headlinequeryURL,
+            }).then(function(response){
+                console.log(response)
+                var printheadline = response.response.docs[0].headline.print_headline;
+                headlines.text('"' + printheadline + '"');
+                headlines.addClass("paper-headlines");
 
-    headlines();  
-})
+                var articleLink = response.response.docs[0].web_url;
+                links.attr('href', articleLink);
+                links.text(articleLink);
+                links.addClass("paper-links");
+
+                headlineArea.addClass("headline-area");
+                headlineArea.append(headlines, links);
+
+                $(".api-image").append(headlineArea);
+            });
--- style-script.js
+++ style-script.js
@@ -1,18 +1,32 @@
 //wait until the page is ready to load
 $( document ).ready(function() {
 
+    //populate the day option drop-down with the days of the month up to 31
+    //for each possible day of the month (31)
     for (var i=0; i<31; i++) {
+        //make an option variable
         var dayOption = $("<option>");
+        //the option variable's text is the current number (starting at 0) + 1 (to have that number start at 1)
         dayOption.text(i+1);
-        if (i<10) {
+        //if the current number is less than 1
+        if (i<9) {
+            //the current value is equal to the current number (starting at 0) + 1
+            //this way all values will have 2 digits when we use them in our api get
             dayOption.val("0"+(i+1));
         }
+        //otherwise there is no 0 in front 
         else {
             dayOption.val(i+1);
         }
-        
+        //add each day option to our input day select
         $("#input-day").append(dayOption);
     }
+
+    //get the last year to use as parameters for our API get
+    //make a variable for this year using moment.js
+    var currentYear = Number(moment().format("YYYY"));
+    //subtract 1 from the current year to get last year
+    lastYear = (currentYear-1);
 
     //object constructor holding the possible inner text for the card as well as the value of the selection
     function cardText (type, lines, paragraph) {
@@ -21,9 +35,13 @@ $( document ).ready(function() {
         this.paragraph = paragraph;
     }
     var cardTextOption = [
-        new cardText ("book", ["Hope your birthday is one for the books!"," Here’s to your next chapter!","Have a happily-ever-after birthday!","Hope it’s lit(erary)!"], "This book was published on the day you were born!"),
-        new cardText ("space", ["You’re out of this world!", "Houston, we have a birthday!", "You've made another voyage around the sun!", "Have a *stellar* birthday!"], "This image was taken by the hubble telescope on the day you were born!"),
-        new cardText ("paper", ["Breaking news, it’s your birthday!", "Experts predict birthday wish comes true!", "I’m very font of you!", "Another birthday? Write on."], "Here is some noteworthy news from the day your were born!"),
+        new cardText ("book", ["Hope your birthday is one for the books!","Here’s to your next chapter!","Have a happily-ever-after birthday!","Hope it’s lit(erary)!"], 
+            "This book was top of the best-seller list on your birthday last year!", ),
+        new cardText ("movie", ["You're a VIP!", "Enjoy your day in the spotlight!", "Lights, camera, happy birthday!"], 
+            "Here is a movie from the year you were born that you may not have seen!", ),
+        new cardText ("space", ["You’re out of this world!", "Houston, we have a birthday!", "You've made another voyage around the sun!", "Have a *stellar* birthday!"], 
+            "This was nasa's image-of-the-day on your birthday last year!"),
+        // new cardText ("paper", ["Breaking news, it’s your birthday!", "Experts predict birthday wish comes true!", "I’m very font of you!", "Another birthday? Write on."], "Here is some noteworthy news from the day your were born!"),
     ];
 
     //when the user submits their information...
@@ -44,39 +62,18 @@ $( document ).ready(function() {
             cardTitle.text("Happy Birthday " + inputName + "!");
             //Make the card title display on the birthday card
             $(".card-title-area").append(cardTitle);
-            console.log(cardTitle.text());
         }
-
-        //get the birthday 
-            //user selected month (from dropdown menu)
-                //the value of the month is from 0-11 so it can be used in a for-loop to get a specific month if wanted
-            var inputMonth = $("#input-month").val();               
-            //user selected day (from dropdown menu)
-            var inputDay = $("#input-day").val();
-            //user input year (typed)
-            var inputYear = $("#input-year").val();
-
-                //maybe if the user input year is not four characters less than 2020 return?? + give a warning? 
-                    
-            console.log(inputMonth + inputDay + inputYear);
-            
-            var nasaQueryURL = "https://api.nasa.gov/planetary/apod?api_key=PxXYhbxjF4qoZihVRPPnN3C4IpvxKF4N1eWiJVRx&date=" +
-                "2019-" +
-                inputMonth + "-" +
-                inputDay;
-            $.ajax({
-                method:"GET",  
-                url: nasaQueryURL,
-            }).then(function(response){
-                console.log(response);
-                console.log (response.hdurl);
-            })
-            makeCard();
+        
+        //run the make card function to add styling to the card
+        makeCard();
+        //run the get api image function to add the api image to our card
+        getAPIImage();
             
 
     });
 
     function makeCard() {
+        //clear out current text
         $(".card-text-area").empty();
 
         //user's selected card style is selected by value of book, space, or paper
@@ -122,6 +119,109 @@ $( document ).ready(function() {
             $(".send-button").removeClass("hide");
             
             $(document).scrollTop($(document).height());
+        }
+    }
+
+    function getAPIImage() {
+        //clear current image on click
+        $(".api-image").empty();
+        //make a variable getting the style choice
+        var cardStyle = $("#card-styles").val();
+        //get the birthday 
+        //user selected month (from dropdown menu) value from 01 - 12
+        var inputMonth = $("#input-month").val();               
+        //user selected day (from dropdown menu)
+        var inputDay = $("#input-day").val();
+        //user input year (typed)
+        var inputYear = $("#input-year").val();
+
+        //make a variable to hold our card image or headlines
+        var cardImage = $("<img>");
+        
+        //make api keys and get functions for each api 
+        //nasa API Function
+        function getNasaImage () {
+            //make query url
+            var nasaURL = "https://api.nasa.gov/planetary/apod?api_key=PxXYhbxjF4qoZihVRPPnN3C4IpvxKF4N1eWiJVRx&date=" +
+                //last year from moment.js
+                lastYear + "-" +
+                //user selected month and day
+                inputMonth + "-" +
+                inputDay;
+            
+            //ajax function using our nasa URL
+            $.ajax({
+                method:"GET",  
+                url: nasaURL,
+            }).then(function(response){
+                //add an src of the nasa high res imagery of the day
+                cardImage.attr("src", response.hdurl);
+                //add an alt
+                cardImage.attr("alt", "space poster image");
+                //apend it to displayed card
+                $(".api-image").append(cardImage);
+            })
+        }
+        //NYT Book API Function
+        function getBookImage() {
+            //make query url
+            var bookURL = "https://api.nytimes.com/svc/books/v3/lists/" +
+                //last year from moment.js
+                lastYear + "-" +
+                //user selected month and day
+                inputMonth + "-" +
+                inputDay +
+                "/hardcover-fiction.json?api-key=PRpWMMh6gJsugEdDfDApTNqb7N7LiRkN";
+            
+            //ajax function using our book URL
+            $.ajax({
+                method:"GET",  
+                url: bookURL,
+            }).then(function(response){
+                var bookCover = response.results.books[0].book_image;
+                //add an src of the book cover image 
+                cardImage.attr("src", bookCover);
+                //add an alt
+                cardImage.attr("alt", "book cover image");
+                //apend it to displayed card
+                $(".api-image").append(cardImage);
+            })
+        }
+        //TMDB API Function
+        function getMovieImage() {
+            //make query URL
+            var movieURL = "https://api.themoviedb.org/3/search/movie?api_key=041542fa1899303e8c187b3a599c9d43&language=en-US&query=e&include_adult=false&primary_release_year=" + 
+                //user selected year    
+                inputYear;
+        
+            //ajax function using our movie URL
+            $.ajax({
+                method:"GET",  
+                url: movieURL,
+            }).then(function(response){
+                //find the movie poster in the API and then use that to make the full image url
+                var movieposter = response.results[0].poster_path;
+                var moviePosterFull = "https://image.tmdb.org/t/p/original" + movieposter;
+                //add an src of the movie poster
+                cardImage.attr("src", moviePosterFull);
+                //add an alt
+                cardImage.attr("alt", "movie poster image");
+                //apend it to displayed card
+                $(".api-image").append(cardImage);
+            })            
+        }
+    
+        //if the user selects "space", run the getNasaImage function
+        if (cardStyle === "space") {
+            getNasaImage();
+        //if the user selects "book", run the getBookImage function
+        } else if (cardStyle === "book") {
+            getBookImage();
+        // } else if (cardStyle === "paper") {
+        //     getHeadlines();
+        //if the user selects anything else (movie is all thats left), run the getMovieImage function
+        }else {
+            getMovieImage();
         }
     }
 });
