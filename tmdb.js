5edfa6a1da9ebdd46d5fc20a1a4e87e72a2f8ea7
Benedicta Kim <61118590+benedictakim@users.noreply.github.com>
2020-07-08 11:22:50 -0400

    finished tmdb.js file

--- tmdb.js
+++ tmdb.js
@@ -1,16 +1,23 @@
-var queryURL = "https://api.themoviedb.org/3/search/movie?api_key=041542fa1899303e8c187b3a599c9d43&query?&language=en-US&include_adult=false&primary_release_year"
+var inputMonth = $("#input-month").val();
+            //user selected day (from dropdown menu)
+            var inputDay = $("#input-day").val();
+            //user input year (typed)
+            var inputYear = $("#input-year").val();
+
+var tmdbqueryURL = "https://api.themoviedb.org/3/search/movie?api_key=041542fa1899303e8c187b3a599c9d43&language=en-US&query=e&include_adult=false&primary_release_year=" + 
+inputYear;
 
 $(document).ready(function(){
     function movies() {
         $.ajax({
             method:"GET",  
-            url: queryURL,
+            url: tmdbqueryURL,
         }).then(function(response){
-            console.log(response) 
-            var movierelease = response.release_date
-            console.log(movierelease)
-            var movieposter = response.poster_path
+            console.log(response)
+            var movieposter = response.results[0].poster_path
             console.log(movieposter)
+            var movieposterfullpath = "https://image.tmdb.org/t/p/original" + movieposter
+            console.log(movieposterfullpath)
         })
     }
     movies();  
